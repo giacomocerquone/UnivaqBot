089fb5af78563df94e835575033b54fbec047163
Giacomo Cerquone <cerquone@gmail.com>
2016-02-13 02:26:04 +0100

    Refactored to 0 pylint's issues

--- cron/professors.py
+++ cron/professors.py
@@ -1,11 +1,20 @@
-import requests
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""This script scrapes all the professors from the univaq website."""
+
 import json
+import requests
+
 from bs4 import BeautifulSoup
 
-def scrapeProfessors():
+def scrape_professors():
+    """Defining the method to scrape professors"""
 
-    # Define url and Headers in order to fake a true browser request
-    professorsUrl = "http://www.disim.univaq.it/didattica/content.php?tipo=3&ordine=1&chiave=0&pid=25&did=8&lid=it&frmRicercaNome=&frmRicercaCognome=&frmRicercaLaurea=1&action_search=Filtra"
+    scraped_professors = []
+    professors_url = "http://www.disim.univaq.it/didattica/" \
+                     "content.php?tipo=3&ordine=1&chiave=0&pid=25&did=8&lid=it&" \
+                     "frmRicercaNome=&frmRicercaCognome=&frmRicercaLaurea=1&action_search=Filtra"
     headers = {
         "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5)",
         "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
@@ -14,38 +23,33 @@ def scrapeProfessors():
         "accept-language": "en-US,en;q=0.8",
     }
 
-    # Make the request to the url passing the headers.
-    request = requests.get(professorsUrl, headers=headers)
+    request = requests.get(professors_url, headers=headers)
 
-    # Check the status code of the response to make sure the request went well
     if request.status_code != 200:
         print("Error! Status "+request.status_code)
         return
 
-    # Pass the whole cached page in 'request' to BeautifulSoup and get the only existent table in the page
-    professorsTable = BeautifulSoup(request.text, "html.parser").find("table")
+    professors_table = BeautifulSoup(request.text, "html.parser").find("table")
 
-    # Array to write into the json file (will be transformed into an array of dictionaries)
-    scrapedProfessors = []
 
-    # These cells are a landmark in the scraping because they have the unique attribute 'colspan=2'
-    firstsTD = professorsTable.find_all(colspan='2')
-    for nameCell in firstsTD:
-        # Scrape the nameCell
-        name = nameCell.find("a").text
-        # Scrape the phone
-        phone = nameCell.find_next_sibling().text
-        # Scrape the e-mail (PROBLEM HERE)
-        email = nameCell.find_next_sibling().find_next_sibling().get_text(".")
-        # Scrape the courses
-        courses = nameCell.find_next_sibling().find_next_sibling().find_next_sibling().get_text(" ").replace('\n', '').replace('\u00a0', '')
+    firsts_td = professors_table.find_all(colspan='2')
+    for name_cell in firsts_td:
+        name = name_cell.find("a").text
+        phone = name_cell.find_next_sibling().text
+        email = name_cell.find_next_sibling().find_next_sibling().get_text(".") # PROBLEM HERE
+        courses = name_cell.find_next_sibling().find_next_sibling().find_next_sibling()\
+                  .get_text(" ").replace('\n', '').replace('\u00a0', '')
 
-        # Finally it appends a dictionary for every element scraped
-        scrapedProfessors.append( {"nome": name, "telefono": phone, "e-mail": email, "corsi": courses, "ufficio": "0"  } )
+        scraped_professors.append({
+            "nome": name,
+            "telefono": phone,
+            "e-mail": email,
+            "corsi": courses,
+            "ufficio": "0"
+        })
 
-    # Write everything to a json file
-    with open('../json/professors.json', 'w') as fp:
-        json.dump(scrapedProfessors, fp)
+    with open('../json/professors.json', 'w') as file_open:
+        json.dump(scraped_professors, file_open)
 
 if __name__ == "__main__":
-    scrapeProfessors()
+    scrape_professors()
