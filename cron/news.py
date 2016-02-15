2bae272b222d93b31ef698c5a8a58934afa3c055
Diego Mariani <diego.mariani@mail.com>
2016-02-15 23:52:00 +0100

    updated news

--- cron/news.py
+++ cron/news.py
@@ -5,18 +5,38 @@
 This is a simple script the schedule the news pull
 """
 
+import json
+import requests
+
 from telegram import Updater
+from bs4 import BeautifulSoup
 
 def build_updater():
-	""" This function build the current job queue updater """
-	return Updater('TOKEN')
+    """ This function build the current job queue updater """
+    return Updater('TOKEN')
 
 def pull_news():
-	""" This function is built to pull all the news from rss endpoint """
-	print "Pulling news"
+    """ This function is built to pull all the news from rss endpoint """
+    pulled_news = []
+    url = "http://www.disim.univaq.it/didattica/content.php?fid=rss&pid=114&did=8&lid=it"
+    headers = {
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5)",
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        "accept-charset": "ISO-8859-1,utf-8;q=0.7,*;q=0.3",
+        "accept-encoding": "gzip,deflate,sdch",
+        "accept-language": "en-US,en;q=0.8",
+    }
+    
+    response = requests.get(url, headers=headers)
 
-def start():
-	"""This is the function that starts the job queue updater"""
-	updater = build_updater()
-    job_queue = updater.job_queue
-    job_queue.put(pull_news, 60, next_t=0, prevent_autostart=True)
+    if response.status_code != 200:
+    	raise ValueError('Something bad happened during news scraping :/')
+    
+    document_root = BeautifulSoup(response.text, "html.parser")
+    items = document_root.findAll("item")
+
+    print items[0]
+
+
+if __name__ == "__main__":
+	pull_news()
