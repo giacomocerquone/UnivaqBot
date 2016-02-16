fc390afccadec6ee63a4085e54da72983c0bc182
Diego Mariani <diego.mariani@mail.com>
2016-02-16 18:06:22 +0100

    changed to feedparser

--- cron/news.py
+++ cron/news.py
@@ -6,7 +6,8 @@ This is a simple script the schedule the news pull
 """
 
 import json
-import requests
+import string
+import feedparser
 
 from bs4 import BeautifulSoup
 
@@ -14,28 +15,14 @@ def pull_news():
     """ This function is built to pull all the news from rss endpoint """
     pulled_news = []
     url = "http://www.disim.univaq.it/didattica/content.php?fid=rss&pid=114&did=8&lid=it"
-    headers = {
-        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5)",
-        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-        "accept-charset": "ISO-8859-1,utf-8;q=0.7,*;q=0.3",
-        "accept-encoding": "gzip,deflate,sdch",
-        "accept-language": "en-US,en;q=0.8",
-    }
 
-    response = requests.get(url, headers=headers)
-
-    if response.status_code != 200:
-        raise ValueError('Something bad happened during news scraping :/')
-
-    document_root = BeautifulSoup(response.text, "html.parser")
-    items = document_root.findAll("item")
+    document_root = feedparser.parse(url)
+    items = document_root["entries"]
 
     for item in items[:10]:
-        title = item.find("title").text
-        description = item.find("description").text
         pulled_news.append({
-            "title": title,
-            "description": description
+            "title": item.title,
+            "description": string.replace(item.description, "&amp;#39;", "'")
             })
 
     with open("../json/news.json", "w") as news_file:
