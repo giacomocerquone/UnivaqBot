c0ecb06b84bd0f1c7d931dbf8e27c684dd02e51d
Diego Mariani <diego.mariani@mail.com>
2016-02-16 18:26:25 +0100

    updated news

--- cron/news.py
+++ cron/news.py
@@ -9,28 +9,22 @@ import json
 import string
 import feedparser
 
-from bs4 import BeautifulSoup
-
 def pull_news():
     """ This function is built to pull all the news from rss endpoint """
-    pulled_news = []
-    url = "http://www.disim.univaq.it/didattica/content.php?fid=rss&pid=114&did=8&lid=it"
 
-    document_root = feedparser.parse(url)
+    pulled_news = []
+    document_root = feedparser.parse(
+        "http://www.disim.univaq.it/didattica/content.php?fid=rss&pid=114&did=8&lid=it"
+        )
     items = document_root["entries"]
 
-    for item in items[:10]:
-        pulled_news.append({
-            "title": item.title,
-            "description": string.replace(item.description, "&amp;#39;", "'")
-            })
+    pulled_news = [
+        {"title": item.title, "description": string.replace(item.description, "&amp;#39;", "'")}
+        for item in items[:10]
+        ]
 
     with open("../json/news.json", "w") as news_file:
         json.dump(pulled_news, news_file)
-
-    print "completed successfully"
-
-
 
 if __name__ == "__main__":
     pull_news()
