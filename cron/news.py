a909e14d43e0d8f98ca95462b47bfdfea7fb0637
Diego Mariani <diego.mariani@mail.com>
2016-02-16 23:37:14 +0100

    improved news

--- cron/news.py
+++ cron/news.py
@@ -11,16 +11,13 @@ import feedparser
 
 def pull_news():
     """ This function is built to pull all the news from rss endpoint """
-
     document_root = feedparser.parse(
         "http://www.disim.univaq.it/didattica/content.php?fid=rss&pid=114&did=8&lid=it"
         )
-
     pulled_news = [
         {"title": item.title, "description": string.replace(item.description, "&amp;#39;", "'")}
         for item in document_root["entries"][:10]
         ]
-
     with open("../json/news.json", "w") as news_file:
         json.dump(pulled_news, news_file)
 
