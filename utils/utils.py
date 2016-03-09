abca8a3ce42afac11079b0dea5335bbd3033535c
Giacomo Cerquone <cerquone@gmail.com>
2016-03-09 12:50:58 +0100

    Implemented new function to retrieve news with beautifulsoup

--- utils/utils.py
+++ utils/utils.py
@@ -6,7 +6,9 @@
 import logging
 import json
 import configparser
-import feedparser
+
+import requests
+from bs4 import BeautifulSoup
 
 def get_configuration():
     """Get global configuration from service.cfg"""
@@ -43,34 +45,53 @@ def read_json(json_file):
         return json.load(json_file)
 
 def pull_news(num):
-    """This function is built to pull 10 (or an arbitrary number) news from the rss endpoint"""
+    """This function is built to pull 10 (or an arbitrary number) news from the news page"""
 
-    document = feedparser.parse(
-        "http://www.disim.univaq.it/didattica/content.php?fid=rss&pid=114&did=8&lid=it")
+    headers = {
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5)",
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        "accept-charset": "ISO-8859-1,utf-8;q=0.7,*;q=0.3",
+        "accept-encoding": "gzip,deflate,sdch",
+        "accept-language": "en-US,en;q=0.8",
+    }
 
+    # Thanks to Luca Pattavina for giving me the right url
+    if num <= 5:
+        news_url = ["http://www.disim.univaq.it/main/news.php?entrant=1"]
+    else:
+        news_url = ["http://www.disim.univaq.it/main/news.php?entrant=1",
+                    "http://www.disim.univaq.it/main/news.php?entrant=2"]
+
+    request = []
+    bs_list = []
     news = []
-    for item in document["entries"][:int(num)]:
-        news.append({
-            "title": item.title,
-            "description": item.description.replace("&amp;#39;", "'"),
-            "id": item.id,
-            "link": item.link
-        })
+    for i, url in enumerate(news_url):
+        request.append(requests.get(url, headers=headers))
+        bs_list.append(BeautifulSoup(request[i].text, "html.parser").find_all(class_="post_item_list"))
+        descr_list = BeautifulSoup(request[i].text, "html.parser").find_all(class_="post_description")
 
-    # WORK IN HERE TO AGGREGATE NEWS WITH BEAUTIFUL SOUP
+        for j, single_news in enumerate(bs_list[i][:int(num)]):
+            news.append({
+                "title": single_news.h3.a.text,
+                "description": descr_list[j].get_text().replace("\n", " "),
+                "id": single_news.a.get('href').split("=")[1],
+                "link": "http://www.disim.univaq.it/main/" + single_news.a.get('href')
+            })
 
     return news
 
 def check_news():
     """This function check if there are some unread news from the website"""
 
-    pulled_news = pull_news(10)
+    pulled_news = pull_news(5)
     stored_news = read_json("json/news.json")
     unread_news = []
 
     for i in range(0, 10):
         if pulled_news[i]["id"] != stored_news[i]["id"]:
             unread_news.append({"title": pulled_news[i]["title"],
-                                "description": pulled_news[i]['description']})
+                                "description": pulled_news[i]['description'],
+                                "id": pulled_news[i]['id'],
+                                "link": pulled_news[i]['link']})
 
     return unread_news
