14d432d4e790e4c9063f4d6a16153e78b40aab95
cclauss <cclauss@bluewin.ch>
2016-03-17 11:18:37 +0100

    Simplify string formatting with str.format(**dict)
    Also use str.join() instead of string concatenation (+).

--- libs/other_commands/other_commands.py
+++ libs/other_commands/other_commands.py
@@ -8,48 +8,35 @@ sys.path.insert(0, '../')
 
 from libs.utils import utils
 
-def prof_command(bot, update, args):
+def prof_command(bot, update, args=None):
     """Defining the `prof` command"""
 
     data = utils.read_json("json/professors.json")
-    professors = ""
-
-    if len(args):
-        for prof in data:
-            if args[0].lower() in prof['nome'].lower():
-                professors += prof['nome'] + \
-                              " - " + prof['telefono'] + \
-                              " - " + prof['e-mail'] + \
-                              " - " + prof['corsi'] + \
-                              "\n\n"
+    prof_name = args[0].strip().lower() if args else ''
+    if prof_name:
+        fmt = '{nome} - {telefono} - {e-mail} - {corsi}\n'
     else:
-        for prof in data:
-            professors += prof['nome'] + \
-                          " - " + prof['telefono'] + \
-                          " - " + prof['e-mail'] + \
-                          "\n"
+        fmt = '{nome} - {telefono} - {e-mail}'
+    professors = '\n'.join(fmt.format(**prof) for prof in data
+                           if prof_name in prof['nome'].lower())
 
-    bot.sendMessage(update.message.chat_id, text=professors)
+    bot.sendMessage(update.message.chat_id, text=professors + '\n')
 
 def student_office_command(bot, update):
     """Defining the `student_office` command"""
 
-    data = utils.read_json("json/student_office.json")
-    student_office_info = "Orari: " + data['orari'] + \
-                          "\nIndirizzo: " + data['indirizzo'] + \
-                          "\nTelefono: " + data['telefono'] + \
-                          "\nE-mail: " + data['e-mail']
-
+    fmt = 'Orari: {orari}\nIndirizzo: {indirizzo}\nTelefono: {telefono}\nE-mail: {e-mail}'
+    student_office_info = fmt.format(**utils.read_json("json/student_office.json"))
     bot.sendMessage(update.message.chat_id, text=student_office_info)
 
 def canteen_command(bot, update):
     """Defining the `canteen` command"""
 
-    data = utils.read_json("json/mensa.json")
-    bot.sendMessage(update.message.chat_id, text="Orari: "+data['orari'])
+    canteen_info = 'Orari: {orari}'.format(**utils.read_json("json/mensa.json"))
+    bot.sendMessage(update.message.chat_id, text=canteen_info)
 
 def adsu_command(bot, update):
-    """Defining the `canteen` command"""
+    """Defining the `adsu` command"""
 
-    data = utils.read_json("json/adsu.json")
-    bot.sendMessage(update.message.chat_id, text=data['info'])
+    adsu_info = utils.read_json("json/adsu.json")['info']
+    bot.sendMessage(update.message.chat_id, text=adsu_info)
