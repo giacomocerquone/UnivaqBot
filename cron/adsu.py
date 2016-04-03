c48e1bfebd1c77ea0cbdbd2be7dfe93576117cc2
Giacomo Cerquone <cerquone@gmail.com>
2016-04-03 07:16:57 +0200

    Linted code and solved a little error

--- cron/adsu.py
+++ cron/adsu.py
@@ -7,14 +7,15 @@ import sys
 sys.path.insert(0, '../')
 from libs.utils import utils
 
-adsu_url = "http://www.adsuaq.org/"
+ADSU_URL = "http://www.adsuaq.org/"
 
-def scrape_adsu(url=adsu_url):
+def scrape_adsu(url=ADSU_URL):
     """Get information about the adsu in a crazy way due to their bitching page made like shit"""
 
     soup = utils.get_soup_from_url(url).find(id="AutoNumber5")
     info = soup.text.replace("  ", "").replace("\t", "").replace("\r", "").replace("\n\n", "")
 
+    scraped_info = {}
     scraped_info.update({
         "info": info
     })
