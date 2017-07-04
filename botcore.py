8eaf1ace5f382aa1564ddd1d8daad614cccde01e
Giacomo Cerquone <cerquone@gmail.com>
2017-07-04 11:07:46 +0200

    Added Stefano Martella contribution for the feedback command

--- botcore.py
+++ botcore.py
@@ -10,10 +10,10 @@ import os
 import telegram
 
 from telegram.ext import Updater, CommandHandler
-from telegram import TelegramError
 
 from libs.utils import utils
 from libs.news_commands import news
+from libs.departments import disim
 from libs.other_commands import other_commands
 
 
@@ -32,74 +32,21 @@ def help_command(bot, update):
 
     help_message = ("La lista di comandi:\n\n"
                     "/help - Stampa questo messaggio\n"
-                    "/news - Leggi le ultime 10 news\n"
-                    "/news num - Leggi le ultime <num> news\n"
-                    "/disimon - Abilita le notifiche per il disim\n"
-                    "/disimoff - Disabilita le notifiche per il disim\n"
+                    "/disim - Leggi le ultime 5 news del disim, /disimon o /disimoff"
+                    " per abilitare o meno le notifiche\n"
                     "/prof - Visualizza tutti i docenti\n"
-                    "/prof cognome - Info su un docente\n"
+                    "/prof cognome/corso - Info su un docente filtrato per corso o cognome\n"
                     "/segreteria - Info sulla segreteria studenti\n"
                     "/mensa - Info sugli orari della mensa\n"
-                    "/adsu - Info sull'adsu"
+                    "/adsu - Info sull'adsu\n"
+                    "/feedback messaggio - Lascia un messaggio agli sviluppatori"
                     "\n\nQuesto bot è orgogliosamente open source"
                     ", sviluppato e mantenuto da Giacomo Cerquone e Stefano Martella.")
 
     bot.sendMessage(update.message.chat_id, text=help_message)
 
 
-def disimon_command(bot, update):
-    """Defining the command to enable notifications for news"""
-
-    if update.message.chat_id not in utils.SUBSCRIBERS:
-        utils.SUBSCRIBERS.append(update.message.chat_id)
-        utils.add_subscriber(update.message.chat_id)
-        bot.sendMessage(update.message.chat_id, text='Notifiche Abilitate!')
-    else:
-        bot.sendMessage(update.message.chat_id,
-                        text='Le notifiche sono già abilitate!')
-
-
-def disimoff_command(bot, update):
-    """Defining the command to disable notifications for news"""
-
-    if update.message.chat_id in utils.SUBSCRIBERS:
-        utils.SUBSCRIBERS.remove(update.message.chat_id)
-        utils.remove_subscriber(update.message.chat_id)
-        bot.sendMessage(update.message.chat_id, text='Notifiche Disattivate!')
-    else:
-        bot.sendMessage(update.message.chat_id,
-                        text='Per disattivare le notifiche dovresti prima attivarle.')
-
-
-def notify_news(bot, job):
-    """Defining method that will be repeated over and over"""
-    unread_news = news.check_news()
-    invalid_chatid = list()
-
-    if unread_news:
-        # need to store only additional news (they are in unread_news)
-        data = news.pull_news(10)
-        news_to_string = ""
-
-        utils.DISIMNEWS = data
-        utils.store_disim_news(data)
-
-        for item in unread_news:
-            news_to_string += "- [{title}]({link})\n{description}\n".format(**item)
-
-        for chat_id in utils.SUBSCRIBERS:
-            try:
-                bot.sendMessage(chat_id, parse_mode='Markdown',
-                                text=news_to_string)
-            except TelegramError:
-                invalid_chatid.append(chat_id)
-
-        for chat_id in invalid_chatid:
-            utils.SUBSCRIBERS.remove(chat_id)
-            utils.remove_subscriber(chat_id)
-
 # Testing
-
 def commands_keyboard(bot, update):
     """Enable a custom keyboard"""
     keyboard = [[]]
@@ -122,24 +69,22 @@ def main():
 
     updater = Updater(token)
     j = updater.job_queue
-    j.run_repeating(notify_news, float(os.environ['NOTIFICATION_INTERVAL']))
+    j.run_repeating(news.notify_news, float(os.environ['NOTIFICATION_INTERVAL']))
     dispatcher = updater.dispatcher
 
     dispatcher.add_handler(CommandHandler("start", start_command))
     dispatcher.add_handler(CommandHandler("help", help_command))
-    dispatcher.add_handler(CommandHandler("news", news.news_command, pass_args=True))
-    dispatcher.add_handler(CommandHandler("disimon", disimon_command))
-    dispatcher.add_handler(CommandHandler("disimoff", disimoff_command))
+    dispatcher.add_handler(CommandHandler("disim", disim.news))
+    dispatcher.add_handler(CommandHandler("disimon", disim.disimon))
+    dispatcher.add_handler(CommandHandler("disimoff", disim.disimoff))
     dispatcher.add_handler(CommandHandler("prof", other_commands.prof_command, pass_args=True))
-    dispatcher.add_handler(CommandHandler(
-        "segreteria", other_commands.student_office_command))
-    dispatcher.add_handler(CommandHandler(
-        "mensa", other_commands.canteen_command))
+    dispatcher.add_handler(CommandHandler("segreteria", other_commands.student_office_command))
+    dispatcher.add_handler(CommandHandler("mensa", other_commands.canteen_command))
     dispatcher.add_handler(CommandHandler("adsu", other_commands.adsu_command))
+    dispatcher.add_handler(CommandHandler("feedback", other_commands.feedback_command))
 
     # Testing
-    dispatcher.add_handler(CommandHandler(
-        "commands_keyboard", commands_keyboard))
+    dispatcher.add_handler(CommandHandler("commands_keyboard", commands_keyboard))
 
     logger.info('Bot started')
 
