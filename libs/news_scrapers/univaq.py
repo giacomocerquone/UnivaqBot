5c9cf78318aa7f69e00531bdabe29b1aad26625a
StefanoMartella <stefano.martella9614@gmail.com>
2017-10-17 15:49:28 +0200

    Adding scraper for univaq.it

---
+++ libs/news_scrapers/univaq.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The actual scraper of Univaq"""
+
+from bs4 import BeautifulSoup
+import requests
+
+def scraper():
+    """This function is built to pull 5 news from the news page of univaq"""
+
+    prefix = "http://www.univaq.it/"
+    news_url = ["http://www.univaq.it/news_archive.php?tipo=In%20evidenza", "http://www.univaq.it/news_archive.php?tipo=Ultimissime"]
+
+    request = []
+    bs_list = []
+    news = { 'In Evidenza': [], 'Ultimissime': [] }
+
+    for i, url in enumerate(news_url):
+        request.append(requests.get(url))
+        bs_list.append(BeautifulSoup(request[i].text, "html.parser").find_all(class_='avviso')[0:5])
+
+        for j, single_news in enumerate(bs_list[i]):
+            if i == 0:
+                news['In Evidenza'].append({
+                    'title': single_news.div.next_sibling.next_sibling.string,
+                    'link': single_news.a['href']
+                })
+            else:
+                news['Ultimissime'].append({
+                    'title': single_news.div.next_sibling.next_sibling.string,
+                    'link': single_news.a['href']
+                })
+
+    return news
